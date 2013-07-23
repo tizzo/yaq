aa5f764f36022d3e86f83686bbd3ef5c5a5c0f66
Howard Tyson <howard@howardtyson.com>
2013-07-23 13:55:42 -0400

    Fleshing out push and pop tests, adding a before to flush the redis database.

--- test/yak-test.js
+++ test/yak-test.js
@@ -7,6 +7,7 @@ describe('Yaq', function() {
   var yaq = null;
   before(function() {
     client = redis.createClient();
+    client.flushdb();
   });
   after(function() {
     client = null;
@@ -29,21 +30,45 @@ describe('Yaq', function() {
     });
     it ('should accept an options array to override defaults', function() {
       yaq = new Yaq(client);
-      yaq.defaultJobTimeout.should.equal(600);
+      yaq.defaultJobTimeout.should.equal(600000);
       yaq = new Yaq(client, { defaultJobTimeout: 10 });
       yaq.defaultJobTimeout.should.equal(10);
     });
   });
   describe('#push', function() {
-    it ('should store an item in the active queue', function(done) {
-      yaq.push('foo', function() {
-        client.lindex(yaq.activeQueueKey, 0, function(error, item) {
-          item.should.equal('"foo"');
-          done(error);
+    describe ('item storage', function() {
+      it ('should store a string in the queue', function(done) {
+        yaq.push('foo', function(error, itemId) {
+          client.lindex(yaq.activeQueueKey, 0, function(error, id) {
+            id.should.equal(itemId);
+            client.get(yaq.itemKeyPrefix + itemId, function(error, record) {
+              record.should.equal('"foo"');
+              done(error);
+            });
+          });
+        });
+      });
+      it ('should store an object in the queue', function(done) {
+        yaq.push({ foo: 'bar' }, function(error, itemId) {
+          client.lindex(yaq.activeQueueKey, 0, function(error, id) {
+            id.should.equal(itemId);
+            client.get(yaq.itemKeyPrefix + itemId, function(error, record) {
+              record.should.equal('{"foo":"bar"}');
+              done(error);
+            });
+          });
         });
       });
     });
   });
   describe('#pop', function() {
+    it('should get the oldest item from the list', function(done) {
+      yaq.pop(function(error, job, timeOut) {
+        job.should.equal('foo');
+        done(error);
+      });
+    });
+  });
+  describe('#bpop', function() {
   });
 });
